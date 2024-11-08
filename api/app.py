bb4062a9a566358f5eb92d99720556f75ad35430
Prateek Pratap Singh <Prateeksingh5116548@gmail.com>
2024-11-08 11:55:30 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 import requests
 import os
@@ -6,28 +6,29 @@ import os
 app = Flask(__name__)
 CORS(app)  
 
-
-
-
 PDL_API_URL = "https://api.peopledatalabs.com/v5/company/enrich"
-
-
 PDL_API_KEY = os.getenv("PDL_API_KEY", "bd16e9c4a343023f4f0b8f4875051131ceb9aba47c470508b52838f16bb46264")
+
+
+@app.after_request
+def add_security_headers(response):
+    # Set Cross-Origin-Opener-Policy and Cross-Origin-Embedder-Policy headers on the response
+    response.headers["Cross-Origin-Opener-Policy"] = "same-origin-allow-popups"
+    response.headers["Cross-Origin-Embedder-Policy"] = "require-corp"
+    return response
+
 
 @app.route('/api/enrich', methods=['POST'])
 def enrich():
-   
     data = request.json
     company_name = data.get('company_name')
     website = data.get('website')
 
-    
     if not company_name:
         return jsonify({'error': 'Company name is required'}), 400
     if not website:
         return jsonify({'error': 'Website URL is required'}), 400
 
-   
     params = {
         'name': company_name,
         'website': website
@@ -38,15 +39,12 @@ def enrich():
         "X-API-Key": PDL_API_KEY
     }
 
-    
     try:
         response = requests.get(PDL_API_URL, headers=headers, params=params)
         response.raise_for_status()  
         company_data = response.json()
-
-        
         return jsonify(company_data), 200
-    except requests.RequestException as e:
+    except requests.RequestException:
         return jsonify({'error': 'Failed to fetch data from People Data Labs'}), 500
 
 if __name__ == '__main__':
